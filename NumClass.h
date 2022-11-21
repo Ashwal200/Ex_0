feabe6ba6958b3956c8ec84ce00599878dd29587
Ron Ashwal <116737896+Ashwal200@users.noreply.github.com>
2022-11-21 18:54:04 +0200

    Ron first commit

---
+++ NumClass.h
@@ -0,0 +1,10 @@
+#ifndef NUMCLASS_H
+#define NUMCLASS_H
+int isArmstrong(int num);
+int isPalindrome(int num);
+int isStrong(int num);
+int factorial(int num);
+int isPrime(int num);
+int isArmstrongByRcurse2(int numToCheck, int nDigits);
+int isPalindromeByRecurse2(int numToCheck, int length);
+#endif 
